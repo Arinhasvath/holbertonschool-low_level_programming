5e6a3c45a4fa027ec98a4c8d0957cba7b72e3b01
Arinhakeo <9295@holbertonstudents.com>
2024-06-18 08:30:38 +0000

    ari

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+/*
+ *
+ *
+ *KIKOOLOL
+ *
+ *
+ *
+ * */
 int main(void)
+
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
