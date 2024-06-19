bd3af1279050256d746f0505b32541022b6f4332
Arinhakeo <9295@holbertonstudents.com>
2024-06-19 16:11:18 +0000

    ari

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 	int last_digit;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	n = rand(98) - RAND_MAX / 2;
 	last_digit = n % 10;
 	printf("last digit of %d is %d and is ", n, last_digit);
 
