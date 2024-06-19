d0de27244c93aa1834f8723d244d8d46a9a9d5e6
Arinhakeo <9295@holbertonstudents.com>
2024-06-19 21:20:10 +0000

    ari

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: ll
+*
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char number;
+
+	for (number = '0'; number <= '10'; number++)
+	{
+	putchar(number);
+	}
+	putchar('\n');
+	return (0);
+}
