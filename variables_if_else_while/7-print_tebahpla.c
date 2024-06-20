c42ab26507637295cdc3a95f79d02beecd3cee58
Arinhakeo <9295@holbertonstudents.com>
2024-06-20 08:23:29 +0000

    ari

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: ll
+*
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int alphabet;
+
+	for (alphabet = 'z'; alphabet <= 'a'; alphabet--)
+	{
+	putchar(alphabet); 
+	}
+	putchar('\n');
+	return (0);
+}
