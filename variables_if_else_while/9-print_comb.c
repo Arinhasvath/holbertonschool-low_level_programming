248d81c45d1b3e166bedb46232f2b75fe31a5642
Arinhakeo <9295@holbertonstudents.com>
2024-06-20 13:35:55 +0000

    ari

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: ll
+*
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int num;
+	int vir;
+	int spas;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+	putchar(num);
+	}
+	for (vir = ','; vir <= ','; vir++)
+	{
+	putchar(vir);
+	}
+	for (spas = ' '; spas <= ' '; spas++)
+	{
+	putchar(spas);
+	}
+	putchar('\n');
+	return (0);
+}
