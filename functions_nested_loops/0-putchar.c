587174f9dab18eb2f9b77d10a4ef0c77d10e3817
Arinhakeo <9295@holbertonstudents.com>
2024-06-24 09:51:02 +0000

    ari

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 * _putchar - writes the character c to stdout
 * @c: The character to print
@@ -8,13 +9,14 @@
 */
 int main(void)
 {
-	_putchar('_')
-	_putchar('p')
-	_putchar('u')
-	_putchar('t')
-	_putchar('c')
-	_putchar('h')
-	_putchar('a')
-	_putchar('r')
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
 	return (0);
 }
