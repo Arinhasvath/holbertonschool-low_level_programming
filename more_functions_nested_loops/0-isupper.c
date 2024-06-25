833305578dd27ab8b369d6d904bc125fcb6bd6ff
Arinhakeo <9295@holbertonstudents.com>
2024-06-25 13:04:58 +0000

    ari

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
-* my_function:  _isppuer
-* @c :test variable
-* return : 1 = uppercase 0 = lower
-* Does my stuff explained.
-* function _isupper
+* _isupper - Vérifie si un caractère est une lettre majuscule.
+* @c: Le caractère à vérifier.
+*
+* Description: Cette fonction prend un seul caractère en entrée et
+* renvoie 1 si le caractère est une lettre majuscule (A-Z), et 0 sinon.
+*
+* Return: 1 si le caractère est majuscule, 0 sinon.
 */
-
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
