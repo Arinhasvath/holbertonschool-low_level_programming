c5f7e5ee13a3551edf4faf4673c6858d1097dc96
Arinhakeo <9295@holbertonstudents.com>
2024-06-26 15:56:42 +0000

    ari

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,12 +1,15 @@
 #include "main.h"
 
-void more_numbers(void);
+void more_numbers(void)
 {
-	char i = 1234567891011121314;
-	
-	for(i = %c *10)
-	
-	printf(%c);
-	
-	_putchar('\n');
+	int i, j;
+	char ligne[] = "1234567891011121314\n";
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 21; j++)
+		{
+			_putchar(ligne[j]);
+		}
+	}
 }
