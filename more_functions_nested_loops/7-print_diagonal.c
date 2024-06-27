d9933a09172536db4941e0b4a1d7546b1b0faf7a
Arinhakeo <9295@holbertonstudents.com>
2024-06-27 13:56:38 +0000

    ari

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,8 +1,22 @@
 #include "main.h"
 void print_diagonal(int n)
 {
-	char n;
-
-	if n = 0;
-
+	int i, j;
+	
+	if (n <= 0)
+{
+	_putchar('\n');
+}
+	else
+		{
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+	{
+	_putchar(' ');
+	}
+	_putchar('\\');
+	_putchar('\n');
+		}
+}
 }
