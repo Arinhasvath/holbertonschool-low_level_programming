4a0b5c3692e11e3b845042da58bb073dfe9a21a0
Arinhakeo <9295@holbertonstudents.com>
2024-06-27 17:51:42 +0000

    betty la

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -2,7 +2,7 @@
 
 void print_square(int size)
 {
-	int i;
+	int i,j;
 
 	if (size <= 0)
 		{
@@ -11,7 +11,10 @@ void print_square(int size)
 	else
 		{
 		for (i = 0; i < size; i++)
+		{
+		for (j = 0; j < size; j++)
 			_putchar('#');
 		}
 	_putchar('\n');
+}
 }
