79da4dde38482e57116bb7ac17ef56d644e0631a
Arinhakeo <9295@holbertonstudents.com>
2024-07-02 19:07:11 +0000

    ari

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,4 +1,20 @@
 #include "main.h"
 
 unsigned int _strspn(char *s, char *accept)
+{ unsigned int nombre = 0;
+	char *i;
 
+	while (*s)
+	{
+		for (i = accept; *i; i++)
+		{
+		if (*s == *i)
+	break;
+		}
+		if (*i =='\0')
+	break;
+		nombre++;
+		s++;
+	}
+	return nombre;
+}
