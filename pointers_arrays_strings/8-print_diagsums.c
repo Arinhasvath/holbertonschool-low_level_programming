234eff1d070013765c06fa13a409a40af7576194
Arinhakeo <9295@holbertonstudents.com>
2024-07-03 19:47:51 +0000

    ari

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,5 +1,27 @@
 #include "main.h"
 
+/**
+ * print_diagsums - Prints the sums of the two diagonals of a square matrix
+ * @a: Pointer to the first element of the matrix
+ * @size: The size of the matrix (number of rows/columns)
+ *
+ * Description: This function calculates and prints the sum of the two
+ * diagonals of a square matrix. The matrix is stored as a 1D array.
+ * The primary diagonal is from the top-left to the bottom-right.
+ * The secondary diagonal is from the top-right to the bottom-left.
+ *
+ * Return: void
+ */
 void print_diagsums(int *a, int size)
+
+	int c3, 3;
+	int c5, 5;
 {
+	c3 = "[1][1] + [2][2] + [3][3]";
+	3 = "[1][3] + [2][2] + [3][1]";
+	c5 = "[1][1] + [2][2] + [3][3] + [4][4] + [5][5]";
+	5 = "[1][5] + [2][4] + [3][3] + [4][2] + [5][1]";
 }
+write(c3, 3);
+write(c5, 5);
+
