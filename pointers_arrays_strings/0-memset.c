d810fba044c72ff6fdb1bef483621a4c978df9cb
Arinhakeo <9295@holbertonstudents.com>
2024-07-03 14:41:44 +0000

    ari

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,6 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+* simple_print_buffer - prints buffer in hexadecimal
+* @buffer: the address of memory to print
+* @size: the size of the memory to print
+*
+* Description: This function prints the content of a memory buffer
+* in hexadecimal format. It prints 10 bytes per line.
+*
+* Return: Nothing.
+*/
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
