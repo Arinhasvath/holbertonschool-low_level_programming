022b1f4864370fe872d3bf14e84d3f616d167a4c
Arinhakeo <9295@holbertonstudents.com>
2024-07-04 21:55:30 +0000

    ari

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - Prints the alphabet 10 times
+*/
+void print_alphabet_x10(void)
+{
+	char alphabet;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		alphabet = 'a';
+
+		while (alphabet <= 'z')
+		{
+			_putchar(alphabet);
+			alphabet++;
+		}
+
+		_putchar('\n');
+	}
+}
