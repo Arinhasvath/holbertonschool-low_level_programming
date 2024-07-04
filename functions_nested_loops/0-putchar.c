4cc63a46012c5ad7c87c24ff49dba0d7a47fcaf5
Arinhakeo <9295@holbertonstudents.com>
2024-07-04 20:53:03 +0000

    ari

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,9 +1,21 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+* main - Entry point
+*
+* Return: Always 0
+*/
 int main(void)
 {
-	char _putchar;
+_putchar('_');
+_putchar('P');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-	return (0);
+return (0);
 }
