cd6d48c7e3cb2fd20f23edd4617fcace9dee8f5e
Arinhakeo <9295@holbertonstudents.com>
2024-07-04 20:29:31 +0000

    ari

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+* _strspn - Gets the length of a prefix substring.
+* @s: The string to be scanned.
+* @accept: The string containing the characters to match.
+*
+* Return: The number of bytes in the initial segment of swhich consist only of bytes from accept.
+*/
 unsigned int _strspn(char *s, char *accept)
 { unsigned int nombre = 0;
 	char *i;
