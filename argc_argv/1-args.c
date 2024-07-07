e6ae118b4a4ed3bb253759aca1ef5283845a4f8d
Arinhakeo <9295@holbertonstudents.com>
2024-07-07 21:03:15 +0000

    ari

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,3 +1,14 @@
 #include <stdio.h>
-
-
+/**
+ * main - Prints the number of arguments passed to the program.
+ * @argc: The total number of arguments passed to the program.
+ * @argv: Array of strings containing the arguments.
+ *
+ * Return: Always 0 (success).
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv; /* void annul argv */
+	printf("%d\n", argc - 1);
+	return (0);
+}
