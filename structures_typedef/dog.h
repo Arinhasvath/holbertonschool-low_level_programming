1e3caa72c75728580288649376f7dc449543d786
Arinhakeo <9295@holbertonstudents.com>
2024-07-10 20:16:37 +0000

    ari

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,10 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+* struct dog - Defines a structure with information about a dog
+* @name: Name of the dog (string)
+* @age: Age of the dog (float)
+* @owner: Owner of the dog (string)
+*/
 struct dog
 {
 	char * name;
 	float age;
 	char * owner;
 };
-#endif
+#endif /* DOG_H */
